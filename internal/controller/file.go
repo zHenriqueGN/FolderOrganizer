4faf239e9307b4c76367b77b4c71b866b3c6058d
zHenriqueGN <henriquegomesnunes19@gmail.com>
2023-08-23 14:22:50 +0000

    feat: create function GetFolderFiles

--- internal/controller/file.go
+++ internal/controller/file.go
@@ -7,6 +7,21 @@ import (
 	"github.com/zHenriqueGN/FolderOrganizer/internal/model"
 )
 
+func GetFolderFiles(folder string) ([]string, error) {
+	var files []string
+	dirEntrys, err := os.ReadDir(folder)
+	if err != nil {
+		return files, err
+	}
+
+	for _, entry := range dirEntrys {
+		if !entry.IsDir() {
+			files = append(files, entry.Name())
+		}
+	}
+	return files, nil
+}
+
 func GenerateFoldersByExtension(dstFolder string, extensions []model.Extension) error {
 	for _, extension := range extensions {
 		folder := path.Join(dstFolder, extension.Name)
