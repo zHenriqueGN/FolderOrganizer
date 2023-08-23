dddbd7a65c93243a208d6aca968b231a35a1e886
zHenriqueGN <henriquegomesnunes19@gmail.com>
2023-08-23 14:38:39 +0000

    feat: generate the folders by extension

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"path/filepath"
+
+	"github.com/zHenriqueGN/FolderOrganizer/internal/controller"
+	"github.com/zHenriqueGN/FolderOrganizer/internal/model"
+)
+
+const (
+	dstFolder = "/workspaces/FolderOrganizer/test"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	files, err := controller.GetFolderFiles(dstFolder)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var extensions = []model.Extension{}
+
+	for _, file := range files {
+		fileExtension := filepath.Ext(file)
+		extension := model.Extension{Name: fileExtension}
+		extension.RemoveDot()
+		extensions = append(extensions, model.Extension{Name: extension.Name})
+	}
+	
+	err = controller.GenerateFoldersByExtension(dstFolder, extensions)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
